0781b7cfcb6051ccd6ef0a0e3eeb6dd09b529e7b
Juan <juandiegovelsol@gmail.com>
2023-06-12 21:45:09 -0500

    Feat: calendar first layout

---
+++ src/assets/data/eventGalery.js
@@ -0,0 +1,27 @@
+import events1 from "../images/eventos1.jpeg";
+import events2 from "../images/eventos2.jpeg";
+import events3 from "../images/eventos3.jpeg";
+import events4 from "../images/eventos4.jpeg";
+import events5 from "../images/eventos5.jpeg";
+import events6 from "../images/eventos6.jpeg";
+
+export const eventCarrousel = [
+  {
+    image: events1,
+  },
+  {
+    image: events2,
+  },
+  {
+    image: events3,
+  },
+  {
+    image: events4,
+  },
+  {
+    image: events5,
+  },
+  {
+    image: events6,
+  },
+];
--- src/pages/ReservePage/ReservePage.jsx
+++ src/pages/ReservePage/ReservePage.jsx
@@ -2,12 +2,26 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectHome, closeMenu, setReserveButton } from "../Home/homeSlice";
 import { NavBar } from "../../components/NavBar";
-import { Menu } from "../../components/Menu";
+import { CustomFooter } from "../../components/CustomFooter";
+import { CarrouselFull } from "../../components/CarrouselFull";
+import { ShadedBottomInfo } from "../../components/ShadedBottomInfo";
+import { ReservationForm } from "../../components/ReservationForm";
+
+import footer from "../../assets/images/footer.jpg";
+import logo from "../../assets/images/logo.png";
+import { socialMedia } from "../../assets/data/homeGalery";
+import { eventCarrousel } from "../../assets/data/eventGalery";
 import "./reserve-page.scss";
 
 const ReservePage = () => {
   const dispatch = useDispatch();
   const { menuHandler } = useSelector(selectHome);
+
+  const handleCloseMenu = () => {
+    if (menuHandler) {
+      dispatch(closeMenu());
+    }
+  };
 
   useEffect(() => {
     dispatch(setReserveButton());
@@ -17,8 +31,21 @@ const ReservePage = () => {
   return (
     <main className="reserve-page">
       <NavBar />
-      {menuHandler && <Menu />}
-      <p>Reserve Page</p>
+      <section onClick={handleCloseMenu} className="menu-page__content">
+        <CarrouselFull carrousel_photos={eventCarrousel}>
+          {" "}
+          {/* cambiar las imagenes, estas son de eventos */}
+          <ShadedBottomInfo text="De El Rancho, con amor" />
+        </CarrouselFull>
+
+        <ReservationForm />
+
+        <CustomFooter
+          logo={logo}
+          background={footer}
+          socialMedia={socialMedia}
+        />
+      </section>
     </main>
   );
 };
