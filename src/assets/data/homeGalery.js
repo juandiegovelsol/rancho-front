b929a6d2f44d956128dc0b96e093dbb61d8cd365
Juan <juandiegovelsol@gmail.com>
2023-06-29 20:41:19 -0500

    Fix: image fixes

--- src/assets/data/homeGalery.js
+++ src/assets/data/homeGalery.js
@@ -1,50 +1,152 @@
-import event1 from "../images/evento1.jpg";
-import event2 from "../images/evento2.jpg";
-import event3 from "../images/evento3.jpg";
+/* import event1 from "../images/evento1.jpg";
+import event2 from "../images/evento2.jpg"; */
+/* import event3 from "../images/evento3.jpg";
 import nature1 from "../images/naturaleza1.jpg";
 import nature2 from "../images/naturaleza2.jpg";
 import romantic from "../images/cena-romantica.jpg";
-import start_dish1 from "../images/entrada-chicharron.jpg";
+import start_dish1 from "../images/entrada-chicharron.jpg"; */
 
 import facebook from "../icons/facebook.svg";
 import instagram from "../icons/instagram.svg";
 import whatsapp from "../icons/whatsapp.svg";
 
-export const homeGalery = [
+import food1 from "../images/gallery/food1.jpeg";
+import food2 from "../images/gallery/food2.jpg";
+import food3 from "../images/gallery/food3.jpg";
+import food4 from "../images/gallery/food4.jpg";
+import food5 from "../images/gallery/food5.jpg";
+import food6 from "../images/gallery/food6.jpg";
+import marriage1 from "../images/gallery/marriage1.jpeg";
+import marriage2 from "../images/gallery/marriage2.jpeg";
+import marriage3 from "../images/gallery/marriage3.jpeg";
+import event1 from "../images/gallery/event1.jpeg";
+import event2 from "../images/gallery/event2.jpeg";
+import birthday1 from "../images/gallery/birthday1.jpeg";
+import birthday2 from "../images/gallery/birthday2.jpeg";
+import birthday3 from "../images/gallery/birthday3.jpeg";
+import birthday4 from "../images/gallery/birthday4.jpeg";
+import birthday5 from "../images/gallery/birthday5.jpeg";
+import nature1 from "../images/gallery/nature1.jpg";
+import nature2 from "../images/gallery/nature2.jpg";
+import nature3 from "../images/gallery/nature3.jpg";
+import nature4 from "../images/gallery/nature4.jpg";
+import nature5 from "../images/gallery/nature5.jpg";
+
+/* export const homeGalery = [
   {
     title: "Matrimonios",
-    text: "La mejor organización de eventos de la región",
     image: event1,
   },
   {
     title: "Entradas",
-    text: "Vive la experiencia de la verdadera comida típica",
     image: start_dish1,
   },
   {
     title: "Bautizos",
-    text: "Deleitate con nuestra mesa de postres",
     image: event2,
   },
   {
     title: "Cena romántica",
-    text: "Consiente tu pareja como tanto se merece",
     image: romantic,
   },
   {
     title: "Naturaleza",
-    text: "Respira la pureza del aire en nuestro ambiente",
     image: nature1,
   },
   {
     title: "Tranquilidad",
-    text: "Descansa y disfruta junto a los tuyos",
     image: nature2,
   },
   {
     title: "Baby Shower",
-    text: "Organizamos todo tipo de eventos, para ti!",
     image: event3,
+  },
+]; */
+
+export const homeGalery = [
+  {
+    title: "Matrimonios",
+    image: marriage1,
+  },
+  {
+    title: "Matrimonios",
+    image: marriage2,
+  },
+  {
+    title: "Matrimonios",
+    image: marriage3,
+  },
+  {
+    title: "Celebraciones",
+    image: event1,
+  },
+  {
+    title: "Celebraciones",
+    image: event2,
+  },
+  {
+    title: "Celebraciones",
+    image: birthday1,
+  },
+  {
+    title: "Celebraciones",
+    image: birthday2,
+  },
+  {
+    title: "Celebraciones",
+    image: birthday3,
+  },
+  {
+    title: "Celebraciones",
+    image: birthday4,
+  },
+  {
+    title: "Celebraciones",
+    image: birthday5,
+  },
+  {
+    title: "Menu eventos",
+    image: food2,
+  },
+  {
+    title: "Menu eventos",
+    image: food5,
+  },
+  {
+    title: "Mesas para eventos",
+    image: food1,
+  },
+  {
+    title: "Nuestros platos",
+    image: food3,
+  },
+  {
+    title: "Nuestros platos",
+    image: food4,
+  },
+  {
+    title: "Nuestros platos",
+    image: food6,
+  },
+  {
+    title: "Nuestros espacios",
+    image: nature1,
+  },
+  {
+    title: "Nuestros espacios",
+    image: nature2,
+  },
+  {
+    title: "Nuestros espacios",
+    image: nature3,
+  },
+  {
+    title: "Nuestros espacios",
+    image: nature4,
+  },
+  {
+    title: "Nuestros espacios",
+    image: nature5,
   },
 ];
 
@@ -59,6 +161,6 @@ export const socialMedia = [
   },
   {
     logo: whatsapp,
-    link: "https://api.whatsapp.com/send?phone=%2B573175108881&text=Hola%2C+quisiera+saber+mas+acerca+de+los+servicios+de+El+Rancho",
+    link: "https://api.whatsapp.com/send?phone=%2B573113182415&text=Hola%2C+quisiera+saber+mas+acerca+de+los+servicios+de+El+Rancho",
   },
 ];
